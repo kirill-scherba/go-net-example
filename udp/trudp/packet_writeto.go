954f7a101d210850d02d53a804ab5851bb0646c1
Kirill Scherba <kirill@scherba.ru>
2019-07-01 19:40:14 +0300

    Comments updated and some extra commented code removed

--- udp/trudp/packet_writeto.go
+++ udp/trudp/packet_writeto.go
@@ -22,14 +22,6 @@ func (packet packetData) writeTo(tcd *channelData) {
 	tcd.sendQueueProcess(func() { tcd.sendQueueAdd(&packet) })
 }
 
-// reWriteTo resend packetData from send queue and update record in send queue
-// \TODO NOT USED
-// func (packet packetData) reWriteTo(tcd *channelData) {
-// 	data := packet.data
-// 	packet.trudp.conn.WriteTo(data, tcd.addr)
-// 	tcd.sendQueueProcess(func() { tcd.sendQueueUpdate(data) })
-// }
-
 // writeTo send packetService to trudp channel
 func (packet packetService) writeTo(tcd *channelData) {
 	data := packet.data
