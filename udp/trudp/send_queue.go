561a9d9d94021f4ce3278d5d4515d141964d45d3
Kirill Scherba <kirill@scherba.ru>
2019-07-02 13:49:32 +0300

    Reset of receive queue created. INITIALIZE and RESET events added to sendvent.

--- udp/trudp/send_queue.go
+++ udp/trudp/send_queue.go
@@ -183,11 +183,10 @@ func (tcd *channelData) sendQueueRemove(packet []byte) {
 	}
 }
 
-// sendQueueReset \TODO reset (clear) send queue
+// sendQueueReset resets (clear) send queue
 func (tcd *channelData) sendQueueReset() {
 	for _, sqd := range tcd.sendQueue {
 		sqd.packet.destroy()
 	}
-	//tcd.sendQueue = nil
 	tcd.sendQueue = tcd.sendQueue[:0]
 }
