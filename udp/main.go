820b0325d72fc0131c8125f8e1b16ab6d1a802df
Kirill Scherba <kirill@scherba.ru>
2019-07-23 20:39:20 +0300

    The sendAnswer parameter added to main application test

--- udp/main.go
+++ udp/main.go
@@ -22,9 +22,10 @@ func main() {
 		maxQueueSize int
 
 		// Control flags
-		noLogTime bool
-		sendTest  bool
-		showStat  bool
+		noLogTime  bool
+		sendTest   bool
+		showStat   bool
+		sendAnswer bool
 	)
 
 	flag.IntVar(&maxQueueSize, "Q", trudp.DefaultQueueSize, "maximum send and receive queues size")
@@ -36,6 +37,8 @@ func main() {
 	flag.StringVar(&logLevel, "log", "CONNECT", "application log level")
 	flag.BoolVar(&sendTest, "send-test", false, "send test data")
 	flag.BoolVar(&showStat, "S", false, "show statistic")
+	flag.BoolVar(&sendAnswer, "answer", false, "send answer")
+
 	flag.Parse()
 
 	tru := trudp.Init(port)
@@ -60,22 +63,20 @@ func main() {
 				tcd.SendTestMsg(sendTest)
 
 				// Sender
-				func() {
-					num := 0
-					const sleepTime = 0
-					for {
-						if sleepTime > 0 {
-							time.Sleep(sleepTime * time.Microsecond)
-						}
-						data := []byte("Hello-" + strconv.Itoa(num) + "!")
-						err := tcd.WriteTo(data)
-						if err != nil {
-							break
-						}
-						num++
+				num := 0
+				const sleepTime = 0
+				for {
+					if sleepTime > 0 {
+						time.Sleep(sleepTime * time.Microsecond)
 					}
-					tru.Log(trudp.CONNECT, "(main) channels sender stopped")
-				}()
+					data := []byte("Hello-" + strconv.Itoa(num) + "!")
+					err := tcd.WriteTo(data)
+					if err != nil {
+						break
+					}
+					num++
+				}
+				tru.Log(trudp.CONNECT, "(main) channels sender stopped")
 
 				time.Sleep(5 * time.Second)
 				tru.Log(trudp.CONNECT, "(main) reconnect")
@@ -86,38 +87,38 @@ func main() {
 	// Receiver
 	go func() {
 		for ev := range tru.ChanEvent() {
-			go func(ev *trudp.EventData) {
-				switch ev.Event {
+			//go func(ev *trudp.EventData) {
+			switch ev.Event {
 
-				case trudp.GOT_DATA:
-					tru.Log(trudp.DEBUG, "(main) GOT_DATA: ", ev.Data, string(ev.Data), fmt.Sprintf("%.3f ms", ev.Tcd.TripTime()))
-					// Send answer if this host not connected to remote hosr
-					if rport == 0 {
-						ev.Tcd.WriteTo([]byte(string(ev.Data) + " - answer"))
-					}
-
-				case trudp.SEND_DATA:
-					tru.Log(trudp.DEBUG, "(main) SEND_DATA:", ev.Data, string(ev.Data))
-
-				case trudp.INITIALIZE:
-					tru.Log(trudp.DEBUG, "(main) INITIALIZE, listen at:", string(ev.Data))
-
-				case trudp.CONNECTED:
-					tru.Log(trudp.CONNECT, "(main) CONNECTED", string(ev.Data))
-
-				case trudp.DISCONNECTED:
-					tru.Log(trudp.CONNECT, "(main) DISCONNECTED", string(ev.Data))
-
-				case trudp.RESET_LOCAL:
-					tru.Log(trudp.CONNECT, "(main) RESET_LOCAL executed at channel:", ev.Tcd.MakeKey())
-
-				case trudp.SEND_RESET:
-					tru.Log(trudp.CONNECT, "(main) SEND_RESET to channel:", ev.Tcd.MakeKey())
-
-				default:
-					tru.Log(trudp.ERROR, "(main)")
+			case trudp.GOT_DATA:
+				tru.Log(trudp.DEBUG, "(main) GOT_DATA: ", ev.Data, string(ev.Data), fmt.Sprintf("%.3f ms", ev.Tcd.TripTime()))
+				// Send answer
+				if sendAnswer {
+					ev.Tcd.WriteTo([]byte(string(ev.Data) + " - answer"))
 				}
-			}(ev)
+
+			case trudp.SEND_DATA:
+				tru.Log(trudp.DEBUG, "(main) SEND_DATA:", ev.Data, string(ev.Data))
+
+			case trudp.INITIALIZE:
+				tru.Log(trudp.DEBUG, "(main) INITIALIZE, listen at:", string(ev.Data))
+
+			case trudp.CONNECTED:
+				tru.Log(trudp.CONNECT, "(main) CONNECTED", string(ev.Data))
+
+			case trudp.DISCONNECTED:
+				tru.Log(trudp.CONNECT, "(main) DISCONNECTED", string(ev.Data))
+
+			case trudp.RESET_LOCAL:
+				tru.Log(trudp.CONNECT, "(main) RESET_LOCAL executed at channel:", ev.Tcd.MakeKey())
+
+			case trudp.SEND_RESET:
+				tru.Log(trudp.CONNECT, "(main) SEND_RESET to channel:", ev.Tcd.MakeKey())
+
+			default:
+				tru.Log(trudp.ERROR, "(main)")
+			}
+			//}(ev)
 		}
 	}()
 
