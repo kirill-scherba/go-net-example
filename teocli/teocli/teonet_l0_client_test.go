c83afd98397a01fc347cb168a99581f49ad39f64
Kirill Scherba <kirill@scherba.ru>
2019-08-24 21:48:27 +0300

    Teocli unit test check and combine two valid packets splitted in one buffer function added

--- teocli/teocli/teonet_l0_client_test.go
+++ teocli/teocli/teonet_l0_client_test.go
@@ -3,7 +3,6 @@ package teocli
 import (
 	"bytes"
 	"fmt"
-	"os"
 	"testing"
 )
 
@@ -20,7 +19,10 @@ func TestPacketCreate(t *testing.T) {
 		if err != nil {
 			t.Errorf("can't create packet, error: %s", err)
 		}
-		fmt.Println("packet:", packet)
+		if packet == nil {
+			t.Errorf("packet does not created")
+		}
+		//fmt.Println("packet:", packet)
 	})
 
 	t.Run("packetCreateString", func(t *testing.T) {
@@ -28,7 +30,10 @@ func TestPacketCreate(t *testing.T) {
 		if err != nil {
 			t.Errorf("can't create packet with string data, error: %s", err)
 		}
-		fmt.Println("packet:", packet)
+		if packet == nil {
+			t.Errorf("packet does not created")
+		}
+		//fmt.Println("packet:", packet)
 	})
 
 	t.Run("packetCreateLogin", func(t *testing.T) {
@@ -36,7 +41,10 @@ func TestPacketCreate(t *testing.T) {
 		if err != nil {
 			t.Errorf("can't create login packet, error: %s", err)
 		}
-		fmt.Println("packet:", packet)
+		if packet == nil {
+			t.Errorf("packet does not created")
+		}
+		//fmt.Println("packet:", packet)
 	})
 
 	t.Run("packetCreateEcho", func(t *testing.T) {
@@ -44,7 +52,10 @@ func TestPacketCreate(t *testing.T) {
 		if err != nil {
 			t.Errorf("can't create echo packet, error: %s", err)
 		}
-		fmt.Println("packet:", packet)
+		if packet == nil {
+			t.Errorf("packet does not created")
+		}
+		//fmt.Println("packet:", packet)
 	})
 
 }
@@ -79,8 +90,12 @@ func TestPacketSend(t *testing.T) {
 	})
 }
 
+// sliceCompare compare two byte slice
 func sliceCompare(pac []byte, packet []byte) bool {
 	if pac == nil && packet == nil {
+		return true
+	}
+	if len(pac) == 0 && packet == nil || pac == nil && len(packet) == 0 {
 		return true
 	}
 	if !(pac != nil && packet != nil && len(pac) == len(packet)) {
@@ -98,6 +113,7 @@ func TestPacketCheck(t *testing.T) {
 
 	// check single valid packet
 	t.Run("validPacket", func(t *testing.T) {
+		teo.ResetReadBuf() // reset read buffer
 		if packet, _ := teo.PacketCreate(cmd, peer, []byte(msg)); packet != nil {
 			pac, status := teo.PacketCheck(packet)
 			if status != 0 {
@@ -111,6 +127,7 @@ func TestPacketCheck(t *testing.T) {
 
 	// check and combine two splitted valid packets
 	t.Run("splittedValidPacket", func(t *testing.T) {
+		teo.ResetReadBuf() // reset read buffer
 		if packet, _ := teo.PacketCreate(cmd, peer, []byte(msg)); packet != nil {
 
 			// Part 1
@@ -145,16 +162,77 @@ func TestPacketCheck(t *testing.T) {
 		}
 	})
 
+	// check and combine two valid packets splitted in one buffer
+	t.Run("splittedValidPacketToBuf", func(t *testing.T) {
+		teo.ResetReadBuf() // reset read buffer
+		packet1, _ := teo.PacketCreate(cmd, peer, append([]byte(msg), 1))
+		packet2, _ := teo.PacketCreate(cmd, peer, append([]byte(msg), 2))
+		if packet1 != nil && packet2 != nil {
+			buf := append(packet1, packet2...)
+			//fmt.Println("combined packet:", buf)
+
+			// Get first valid packet from buffer (now the teocli read buffer is
+			// empty and input function buffer contains two valid packet).
+			// The PacketCheck return one valid teocli packet and save next one to
+			// read buffer
+			pac, status := teo.PacketCheck(buf)
+			if status != 0 {
+				t.Errorf("return wrong status %d for valid packet", status)
+			}
+			if pac == nil {
+				t.Errorf("return wrong packet")
+			}
+			if !sliceCompare(pac, packet1) {
+				t.Errorf("\n"+
+					"a packet: %v\n"+
+					"wait for: %v", pac, packet1)
+			}
+			//fmt.Println("first packet:   ", pac)
+
+			// Get next valid packet saved in teocli read buffer (now the teocli read
+			// buffer contains one valid packet and input function buffer is nil).
+			// The PacketCheck return one valid teocli packet from read buffer and
+			// free it
+			pac, status = teo.PacketCheck(nil)
+			if status != 0 {
+				t.Errorf("return wrong status %d for valid packet", status)
+			}
+			if pac == nil {
+				t.Errorf("return wrong packet")
+			}
+			if !sliceCompare(pac, packet2) {
+				t.Errorf("\n"+
+					"a packet: %v\n"+
+					"wait for: %v", pac, packet1)
+			}
+			//fmt.Println("second packet:  ", pac)
+
+			// Try to get next packet - result should be nil, read buffer - nil
+			pac, status = teo.PacketCheck(nil)
+			if status != -1 {
+				t.Errorf("return wrong status %d for empty packet", status)
+			}
+			if pac != nil {
+				t.Errorf("return wrong packet")
+			}
+			if !sliceCompare(teo.readBuffer, nil) {
+				t.Errorf("wrong content of teocli read buffer: %v", teo.readBuffer)
+			}
+
+		}
+	})
+
 	// check and combine two splitted invalid packets
 	t.Run("splittedInvalidPacket", func(t *testing.T) {
+		teo.ResetReadBuf() // reset read buffer
 		if packet, _ := teo.PacketCreate(cmd, peer, []byte(msg)); packet != nil {
 			packet1 := packet[:20]
 			pac, status := teo.PacketCheck(packet1)
 			if status != -1 {
-				t.Errorf("return wrong status %d for valid packet", status)
+				t.Errorf("return wrong status %d for invalid packet", status)
 			}
-			if !sliceCompare(pac, nil) {
-				t.Errorf("return wrong packet")
+			if pac != nil {
+				t.Errorf("return wrong %d bytes packet: %v", len(pac), pac)
 			}
 
 			packet2 := packet[10:]
@@ -170,6 +248,7 @@ func TestPacketCheck(t *testing.T) {
 
 	// check and combine splitted 9 invalid packets with length = 1
 	t.Run("splittedInvalidPacketSmallFirst", func(t *testing.T) {
+		teo.ResetReadBuf() // reset read buffer
 		if packet, _ := teo.PacketCreate(cmd, peer, []byte(msg)); packet != nil {
 			packet1 := []byte("z")
 			waitStatus := -1
@@ -195,6 +274,6 @@ func TestBytes(t *testing.T) {
 	var b bytes.Buffer // A Buffer needs no initialization.
 	b.Write([]byte("Hello "))
 	fmt.Fprintf(&b, "world!")
-	b.WriteTo(os.Stdout)
-
+	//b.WriteTo(os.Stdout)
+	sliceCompare(b.Bytes(), []byte("Hello world!"))
 }
