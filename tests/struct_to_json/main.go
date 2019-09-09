efe9b3b3cddbc442afad2abca7a7a2e7c6b73ff0
Kirill Scherba <kirill@scherba.ru>
2019-09-09 17:13:14 +0300

    Struct to json test added

---
+++ tests/struct_to_json/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
+func StructToJSON(data interface{}) ([]byte, error) {
+	buf := new(bytes.Buffer)
+
+	//fmt.Printf("%v\n", data)
+	if err := json.NewEncoder(buf).Encode(data); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func main() {
+
+	var data = struct {
+		IVal int    `json:"i_val"`
+		SVal string `json:"s_val"`
+	}{
+		25,
+		"this is 25",
+	}
+
+	jsonBytes, err := StructToJSON(data)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Printf("%s\n", string(jsonBytes))
+}
