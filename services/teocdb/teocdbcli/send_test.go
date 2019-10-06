5e3d10ac7f180810aca6f4d32057576d6a7ad55d
Kirill Scherba <kirill@scherba.ru>
2019-10-06 13:09:53 +0300

    The TestSend code refactoring

--- services/teocdb/teocdbcli/send_test.go
+++ services/teocdb/teocdbcli/send_test.go
@@ -22,86 +22,87 @@ func TestSend(t *testing.T) {
 	param.Name = "teo-test"
 	param.LogLevel = "NONE"
 	teo := teonet.Connect(param, []string{"teo-go", "teo-test"}, version)
+	testSend := func(pac *teonet.Packet) {
+		// Create new teonet teocdb client
+		cdb := NewTeocdbCli(teo)
+
+		fmt.Printf("connected to %s\n", pac.From())
+
+		// Key value which will be used in test
+		key := "test.key.919"
+		value := "Hello world-919-23! - " + fmt.Sprint(time.Now())
+
+		// Error processing function
+		error := func(err error) {
+			fmt.Printf("got error: %s\n", err.Error())
+			t.Error(err)
+			ch <- false
+		}
+
+		// Set data - Save {key,value} data to DB
+		fmt.Printf("Set data: %s\n", value)
+		data, err := cdb.Send(CmdSet, key, []byte(value))
+		if err != nil {
+			error(err)
+			return
+		}
+		fmt.Printf("got answer data: %v\n", data)
+
+		// Get data - Read data from DB by key
+		fmt.Printf("Get data\n")
+		data, err = cdb.Send(CmdGet, key)
+		if err != nil {
+			error(err)
+			return
+		}
+		fmt.Printf("got answer data: %s\n", string(data))
+		if string(data) != value {
+			error(errors.New("got wrong answer data"))
+			return
+		}
+
+		// Get not existing key
+		fmt.Printf("Get not existing data\n")
+		data, err = cdb.Send(CmdGet, "a.not.existing.key.test")
+		if err != nil {
+			error(err)
+			return
+		}
+		fmt.Printf("got answer data: %v\n", data)
+
+		// Get list of keys - Read array of keys with common prefix
+		fmt.Printf("Get list\n")
+		data, err = cdb.Send(CmdList, "test.key.")
+		if err != nil {
+			error(err)
+			return
+		}
+		//keylist, err := cdb.Keys(data)
+		var keylist KeyList
+		keylist.UnmarshalBinary(data)
+		fmt.Printf("got answer keys: %v\n%v\n", keylist, data)
+
+		// Get values of all keys received in previous example
+		for _, key := range keylist.Keys() {
+			data, err = cdb.Send(CmdGet, key)
+			if err != nil {
+				error(err)
+				return
+			}
+			fmt.Printf("got answer key, value: %s -> %s\n", key,
+				string(data))
+		}
+
+		ch <- true
+	}
 	go teo.Run(func(teo *teonet.Teonet) {
 		for ev := range teo.Event() {
 			switch ev.Event {
 			case teonet.EventConnected:
 				pac := ev.Data
-				// Start cdb test after peer teo-cdb connected to this test
+				// Start cdb test after peer teo-cdb connected to this app
 				if pac.From() == "teo-cdb" {
-					go func() {
-						// Create new teonet teocdb client
-						cdb := NewTeocdbCli(teo)
-
-						fmt.Printf("connected to %s\n", pac.From())
-
-						// Key value which will be used in test
-						key := "test.key.919"
-						value := "Hello world-919-23! - " + fmt.Sprint(time.Now())
-
-						// Error processing function
-						error := func(err error) {
-							fmt.Printf("got error: %s\n", err.Error())
-							t.Error(err)
-							ch <- false
-						}
-
-						// Set data - Save {key,value} data to DB
-						fmt.Printf("Set data: %s\n", value)
-						data, err := cdb.Send(CmdSet, key, []byte(value))
-						if err != nil {
-							error(err)
-							return
-						}
-						fmt.Printf("got answer data: %v\n", data)
-
-						// Get data - Read data from DB by key
-						fmt.Printf("Get data\n")
-						data, err = cdb.Send(CmdGet, key)
-						if err != nil {
-							error(err)
-							return
-						}
-						fmt.Printf("got answer data: %s\n", string(data))
-						if string(data) != value {
-							error(errors.New("got wrong answer data"))
-							return
-						}
-
-						// Get not existing key
-						fmt.Printf("Get not existing data\n")
-						data, err = cdb.Send(CmdGet, "a.not.existing.key.test")
-						if err != nil {
-							error(err)
-							return
-						}
-						fmt.Printf("got answer data: %v\n", data)
-
-						// Get list of keys - Read array of keys with common prefix
-						fmt.Printf("Get list\n")
-						data, err = cdb.Send(CmdList, "test.key.")
-						if err != nil {
-							error(err)
-							return
-						}
-						//keylist, err := cdb.Keys(data)
-						var keylist KeyList
-						keylist.UnmarshalBinary(data)
-						fmt.Printf("got answer keys: %v\n%v\n", keylist, data)
-
-						// Get values of all keys received in previous example
-						for _, key := range keylist.Keys() {
-							data, err = cdb.Send(CmdGet, key)
-							if err != nil {
-								error(err)
-								return
-							}
-							fmt.Printf("got answer key, value: %s -> %s\n", key,
-								string(data))
-						}
-
-						ch <- true
-					}()
+					go testSend(pac)
 				}
 			}
 		}
