7cab4514d7645ec5eb43a9bac39e7aada44f7f8e
Kirill Scherba <kirill@scherba.ru>
2019-10-12 01:17:42 +0300

    Package teonet comment updated

--- teonet/teonet/teonet.go
+++ teonet/teonet/teonet.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package teonet contain Teonet server functions and data structures
+// Package teonet contain Teonet server functions and data structures.
 package teonet
 
 // #include "packet.h"
