e464114278be0f74002891dceb57ad3da19d1ac7
Kirill Scherba <kirill@scherba.ru>
2019-10-19 04:48:26 +0300

    Config using syntax updated

--- teocli/examples/teocli-termloop/config.go
+++ teocli/examples/teocli-termloop/config.go
@@ -1,10 +1,26 @@
 package main
 
-import "os"
+import (
+	"os"
 
-// Config is teonet config
-type Config struct {
+	"github.com/kirill-scherba/teonet-go/services/teoconf"
+)
+
+// Parameters is teocli-termloop teonet connection config structure
+type Parameters struct {
 	Cookies string
+}
+
+// Config contain methods and stucture with config data
+type Config struct {
+	Parameters
+	configName string
+}
+
+// NewConfig initialize config
+func newConfig(name string) (*teoconf.Teoconf, *Parameters) {
+	conf := &Config{configName: name}
+	return teoconf.New(conf), &conf.Parameters
 }
 
 // Default set config default values
@@ -12,17 +28,17 @@ func (c *Config) Default() []byte {
 	return nil
 }
 
+// Value return config value interface
+func (c *Config) Value() interface{} {
+	return &c.Parameters
+}
+
+// Name return config name
+func (c *Config) Name() string {
+	return "termloop-" + c.configName
+}
+
 // Dir return config directory
 func (c *Config) Dir() string {
 	return os.Getenv("HOME") + "/.config/teonet/teocli-termloop/"
-}
-
-// FileName return config directory
-func (c *Config) FileName() string {
-	return "termloop"
-}
-
-// Struct return config directory
-func (c *Config) Struct() interface{} {
-	return c
 }
